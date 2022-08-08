58d26cb1d4bb21a71c89dcec417b661232e3f7e1
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-08 20:55:42 -0300

    Init

---
+++ SOCIALMEDIA/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Cover from '../../img/cover.jpg';
+import Profile from '../../img/profileImg.jpg';
+import './ProfileCard.css';
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
+
+      <div className="ProfileName">
+        <span>Zendaja MJ</span>
+        <span>SENIOR UX/IX Designer</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
