b4359ecda58abe9b0785564c11900ac03b11b020
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-09 12:15:36 -0300

    Finish prototype of profileCard

--- SOCIALMEDIA/src/components/ProfileCard/ProfileCard.jsx
+++ SOCIALMEDIA/src/components/ProfileCard/ProfileCard.jsx
@@ -18,7 +18,21 @@ const ProfileCard = () => {
 
       <div className="followStatus">
         <hr />
+        <div>
+          <div className="follow">
+            <span>6,890</span>
+            <span>Following </span>
+          </div>
+          <div className="vl"></div>
+
+          <div className="follow">
+            <span>1</span>
+            <span>Followers </span>
+          </div>
+        </div>
+        <hr />
       </div>
+      <span>My Profile</span>
     </div>
   );
 };
