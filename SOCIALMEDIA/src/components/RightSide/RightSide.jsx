b7adea639c55113e48492bab303420e9b92d09d8
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-10 15:55:36 -0300

    2.O

--- SOCIALMEDIA/src/components/RightSide/RightSide.jsx
+++ SOCIALMEDIA/src/components/RightSide/RightSide.jsx
@@ -5,10 +5,12 @@ import Noti from '../../img/noti.png';
 import Comment from '../../img/comment.png';
 import { UilSetting } from '@iconscout/react-unicons';
 import TrendCard from '../TrendCard/TrendCard';
+import ShareModal from '../ShareModal/ShareModal';
 // import TrendCard from '../TrendCard/TrendCard';
 // import ShareModal from '../ShareModal/ShareModal';
 
 const RightSide = () => {
+  const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="RightSide">
       <div className="navIcons">
@@ -20,7 +22,10 @@ const RightSide = () => {
 
       <TrendCard />
 
-      <button className="button r-button">Share</button>
+      <button className="button r-button" onClick={() => setModalOpened(true)}>
+        Share
+      </button>
+      <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   );
 };
