698057f098bb28b83acaac6e15e531a02e40f95b
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-11 19:31:04 -0300

    DELETE POST LOGIC

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { createPost } from '../Controllers/PostController.js';
 import { getPost } from '../Controllers/PostController.js';
 import { updatePost } from '../Controllers/PostController.js';
+import { deletePost } from '../Controllers/PostController.js';
 
 const router = express.Router();
 
 router.post('/', createPost);
 router.get('/:id', getPost);
 router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
