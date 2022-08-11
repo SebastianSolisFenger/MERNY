de868574b622f9f345d1f3cee38c1de188ffa287
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-11 18:42:00 -0300

    Create GET => POSTS

--- server/Models/postModel.js
+++ server/Models/postModel.js
@@ -1,21 +1,4 @@
 // @ts-nocheck
-// import mongoose from 'mongoose';
-
-// const postSchema = new mongoose.Model(
-//   {
-//     userId: {
-//       type: String,
-//       required: true,
-//     },
-//     desc: String,
-//     likes: [],
-//     image: String,
-//   },
-//   { timestamps: true }
-// );
-
-// const PostModel = mongoose.model('Posts', postSchema);
-// export default PostModel;
 
 import mongoose from 'mongoose';
 
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { createPost } from '../Controllers/PostController.js';
+import { getPost } from '../Controllers/PostController.js';
 
 const router = express.Router();
 
 router.post('/', createPost);
+router.get('/:id', getPost);
 
 export default router;
