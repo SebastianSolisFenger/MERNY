97548c64a47eb17aa878c9698f17233bc0b4a120
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-11 19:48:10 -0300

    CONTROLLER =>  Like // dislike posts

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -71,3 +71,26 @@ export const deletePost = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+// ------------------ Like // dislike posts  ------------------//
+
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+
+    if (!post.likes.includes(userId)) {
+      await post?.updateOne({
+        $push: {
+          likes: userId,
+        },
+      });
+      res.status(200).json('Post liked successfully');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -3,6 +3,7 @@ import { createPost } from '../Controllers/PostController.js';
 import { getPost } from '../Controllers/PostController.js';
 import { updatePost } from '../Controllers/PostController.js';
 import { deletePost } from '../Controllers/PostController.js';
+import { likePost } from '../Controllers/PostController.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.post('/', createPost);
 router.get('/:id', getPost);
 router.put('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.put('/:id/like', likePost);
 
 export default router;
