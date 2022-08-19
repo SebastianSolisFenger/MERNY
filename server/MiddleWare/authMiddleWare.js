eb400ed55db3ea639bbd9d55c7ff691522ddabdc
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-19 19:02:39 -0300

    Doesnt seems to be working

---
+++ server/MiddleWare/authMiddleWare.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.JWT_KEY;
+
+const authMiddleWare = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+
+    console.log(token);
+
+    if (token) {
+      const decoded = jwt.verify(token, secret);
+      console.log(decoded);
+      req.body._id = decoded?._id;
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default authMiddleWare;
