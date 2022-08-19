d7f84f6521484343c6b931008af7f4086010312c
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-19 17:29:00 -0300

    UNFOLLOW & FOLLOW USER CASE =>  DONT FULLY UNDERSTOOD

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -30,6 +30,34 @@ const authReducer = (
     case 'UPDATING_FAIL':
       return { ...state, updateLoading: false, error: true };
 
+    // FOLLOW USER
+    case 'FOLLOW_USER':
+      return {
+        ...state,
+        authData: {
+          ...state.authData,
+          user: {
+            ...state.authData.user,
+            following: [...state.authData.user.following, action.data],
+          },
+        },
+      };
+
+    // UNFOLLOW USER
+    case 'UNFOLLOW_USER':
+      return {
+        ...state,
+        authData: {
+          ...state.authData,
+          user: {
+            ...state.authData.user,
+            following: state.authData.user.following.filter(
+              (personId) => personId !== action.data
+            ),
+          },
+        },
+      };
+
     case 'LOG_OUT':
       localStorage.clear();
       return { ...state, authData: null, loading: false, error: false };
