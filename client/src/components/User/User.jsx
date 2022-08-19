26cba7777018f6d6f76bf07dd19c78f07607ff60
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-19 17:22:25 -0300

    Created HANDLEFOLLOW function

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useReducer } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { unfollowUser } from '../../actions/userAction';
 
 const User = ({ person }) => {
   const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+
+  // CHECK IF THE AUTH USER IS FOLLOWING THE PERSON
+  const [following, setFollowing] = useState(
+    person.followers.includes(user._id)
+  );
+
+  // HANLDE FOLLOW
+  const handleFollow = () => {
+    dispatch(unfollowUser(person._id, user));
+  };
 
   return (
     <div className="follower">
@@ -16,11 +31,13 @@ const User = ({ person }) => {
           className="followerImg"
         />
         <div className="name">
-          <span>{person.name}</span>
+          <span>{person.firstname}</span>
           <span>{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button className="button fc-button" onClick={handleFollow}>
+        {following ? 'Unfollow' : 'Follow'}
+      </button>
     </div>
   );
 };
